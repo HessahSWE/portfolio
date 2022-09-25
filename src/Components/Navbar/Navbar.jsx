a882e2b3014897a3dfc9e9a4453a9c468385f4db
HessahSWE <hissahalhussain@gmail.com>
2022-09-26 00:26:32 +0300

    update

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,24 +1,25 @@
 import React from "react";
 import './Navbar.css';
-const Navbar = () =>{
+
+const Navbar = () => {
   return (
     <div className="n-wrapper">
       <div className="n-left">
-      <div className="n-name">Hessah</div>
-      <span>toggle</span>
+        <div className="n-name">Hessah</div>
+        <span>toggle</span>
       </div>
       <div className="n-right">
-      <div className="n-list">
-      <ul>
-        <li>Home</li>
-        <li>Services</li>
-        <li>Experience</li>
-        <li>Portfolio</li>
-        <li>Testimonials</li>
-      </ul>
-    </div> 
-    <button className="button">Contact Us</button>
-    </div></div>
+        <div className="n-list">
+          <ul >
+            <li>Home</li>
+            <li>Services</li>
+            <li>Experience</li>
+            <li>Portfolio</li>
+            <li>Testimonials</li>
+          </ul>
+        </div>
+        <button className="button">Contact Us</button>
+      </div></div>
   )
 }
 export default Navbar;
