52f509bd533b3fb5e218a3f144f8ff524e1118ff
HessahSWE <hissahalhussain@gmail.com>
2022-09-26 00:57:48 +0300

    update

---
+++ src/Components/Intro/Intro.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+const Intro = () => {
+  return (
+    <div className="intro">
+      <div className="i-left">
+        <div className="i-name">
+          <span>Welcome! I Am </span>
+          <span>Hessah</span>
+          <span> Software Engineer with high level of experience in development
+              , producing the Quality work</span>
+        </div>
+        <button className="button i-button">Hire me</button>
+      </div>
+      <div className="i-right"></div>
+    </div>
+  );
+}
+export default Intro;
