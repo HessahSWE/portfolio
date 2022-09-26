cb8720b30527b69b865270133e29acd35004f393
HessahSWE <hissahalhussain@gmail.com>
2022-09-27 01:21:23 +0300

    update

--- src/Components/Intro/Intro.jsx
+++ src/Components/Intro/Intro.jsx
@@ -1,17 +1,18 @@
 import React from "react";
+import './Intro.css';
 const Intro = () => {
   return (
     <div className="intro">
       <div className="i-left">
         <div className="i-name">
-          <span>Welcome! I Am </span>
+          <span>Welcome! I Am</span>
           <span>Hessah</span>
-          <span> Software Engineer with high level of experience in development
-              , producing the Quality work</span>
+          <span>Software Engineer with high level of experience in development
+            , producing the Quality work</span>
         </div>
         <button className="button i-button">Hire me</button>
       </div>
-      <div className="i-right"></div>
+      <div className="i-right">I am right side</div>
     </div>
   );
 }
